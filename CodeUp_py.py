8b316cf72531acc1ac91522a1f4c40f2e3eced12
yalu-ku <74523709+yalu-ku@users.noreply.github.com>
2021-06-29 22:25:24 +0900

    Add files via upload

---
+++ CodeUp_py.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# 6009
+a = input()
+print(a)
+
+
+# In[2]:
+
+
+#6010
+b = int(input())
+print(b)
+
+
+# In[4]:
+
+
+#6011
+c = float(input())
+print(c)
+
